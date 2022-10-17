57f13f42373c8d79a4fb69bffc108f7957e611a9
LucasVianaSampaio <lv6612530@gmail.com>
2022-10-17 19:10:01 -0300

    Questão 6

---
+++ ex06.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 10
+
+void poe_nome(char *, char *);
+void troca(char *, char *, char *);
+
+int main()
+{
+  char nome[TAM];
+  char sobrenome[TAM];
+  char aux[TAM];
+
+  poe_nome(&nome, &sobrenome);
+
+  troca(&nome, &sobrenome, &aux);
+
+  return 0;
+}
+
+void poe_nome(char *pnome, char *psobrenome)
+{
+  printf("Digite seu primeiro nome abaixo:\n");
+  fgets(pnome, TAM, stdin);
+  printf("Digite seu segundo nome abaixo:\n");
+  fgets(psobrenome, TAM, stdin);
+}
+
+void troca(char *pnome, char *psobrenome, char *paux)
+{
+  strcpy(paux, pnome);
+  strcpy(pnome, psobrenome);
+  strcpy(psobrenome, paux);
+
+  printf("Seu nome é: %s", pnome);
+  printf("Seu sobrenome é: %s", psobrenome);
+}
